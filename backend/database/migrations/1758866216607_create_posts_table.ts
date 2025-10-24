66cc9011eaefa3e4687daed04217266774e24962
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-10-24 19:20:11 +0600

    fix: change 'content' column to nullable in posts table migration

--- backend/database/migrations/1758866216607_create_posts_table.ts
+++ backend/database/migrations/1758866216607_create_posts_table.ts
@@ -7,7 +7,7 @@ export default class Posts extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
-      table.text('content').notNullable()
+      table.text('content').nullable()
       table.string('image_url').nullable()
       table.string('video_url').nullable()
       table.timestamps(true)
